7e56892db85bc22d0e2cd20b2cb9ec10e7b283bd
jorgegomezdi <106641101+jorgegomezdi@users.noreply.github.com>
2022-06-14 23:59:59 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package com.ejercicio;
+
+public class Main {
+    public static void main(String[] args) {
+        String palabra = "Hola Mundo";
+        char letra = 'A';
+        int numeroInt = 10;
+        double numeroDouble = 25.4;
+        byte numeroByte = 127;
+        short numeroShort = 32767;
+        long numeroLong = 65555656868L;
+        float numeroFloat = 1.25F;
+        boolean bool = false;
+
+        System.out.println("String: " + palabra);
+        System.out.println("Char: " + letra);
+        System.out.println("Int: " + numeroInt);
+        System.out.println("Double: " + numeroDouble);
+        System.out.println("Byte: " + numeroByte);
+        System.out.println("Short: " + numeroShort);
+        System.out.println("Long: " + numeroLong);
+        System.out.println("Float: " + numeroFloat);
+        System.out.println("Boolean: " + bool);
+    }
+
+}
